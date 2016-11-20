743dde2f8ff57e4f84d821b79233c7c518fb4bbc
Gergo Miho <gergo.miho@gmail.com>
2016-11-20 19:04:57 +0100

    Add project files.

---
+++ AI/Entities.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI
+{
+    class Entities
+    {
+        Random rnd;
+        private int itsAge;
+        private int maxAge;
+        private bool itsGender;
+        private double itsBreedingRate;
+        private double itsFightingRate;
+        private int itsStrength;
+        private int itsHealth;
+        private int maxChildren;
+        private int maxFight;
+        private bool bIsAlive;
+
+        public bool isAlive()
+        {
+            return bIsAlive;
+        }
+
+        public bool isMale()
+        {
+            return itsGender;
+        }
+
+        public void doAge()
+        {
+            if (this.bIsAlive)
+            {
+                this.itsAge++;
+                if (this.itsAge > this.maxAge)
+                    bIsAlive = false;
+            }
+        }
+
+        public Entities doBreed(Entities context)
+        {
+            if (this.bIsAlive && context.bIsAlive)
+            {
+                if (this.itsGender != context.itsGender)
+                {
+                    if (this.itsBreedingRate >= rnd.NextDouble() && context.itsBreedingRate >= context.rnd.NextDouble())
+                    {
+                        Console.WriteLine("Breeding !");
+                        return new Entities();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public void doFight(Entities context)
+        {
+            if (this.bIsAlive && context.bIsAlive)
+            {
+                if (this.itsFightingRate >= rnd.NextDouble() && context.itsFightingRate >= context.rnd.NextDouble())
+                {
+                    Console.WriteLine("Fighting !");
+                    if (this.itsStrength > context.itsStrength)
+                        context.doInjury(this.itsStrength - context.itsStrength);
+                    else if (this.itsStrength > context.itsStrength)
+                        this.doInjury(context.itsStrength - this.itsStrength);
+                    else if (this.itsStrength == context.itsStrength)
+                    {
+                        this.doInjury(rnd.Next(0, this.itsHealth));
+                        context.doInjury(context.rnd.Next(0, context.itsHealth));
+                    }
+                }
+            }
+        }
+
+        private void doInjury(int damage)
+        {
+            Console.WriteLine("Damage done : {0} !",damage);
+            this.itsHealth -= damage;
+            if (this.itsHealth <= 0)
+            {
+                bIsAlive = false;
+                Console.WriteLine("Entity died !");
+            }
+        }
+
+        public Entities()
+        {
+            rnd = new Random(Guid.NewGuid().GetHashCode());
+            itsAge = 0;
+            maxAge = rnd.Next(60, 80);
+            maxChildren = rnd.Next(0, 5);
+            maxFight = rnd.Next(0, 10);
+            itsGender = rnd.Next(0, 100) >= 50 ? true : false;
+            itsBreedingRate = rnd.NextDouble()/maxAge*maxChildren;
+            itsFightingRate = rnd.NextDouble()/maxAge*maxFight;
+            itsStrength = rnd.Next(0, 100);
+            itsHealth = rnd.Next(80, 100);
+            bIsAlive = true;
+
+            Console.WriteLine("Entity has born !");
+        }
+
+        ~Entities()
+        {
+            Console.WriteLine("Entity has died !");
+        }
+    }
+}
---
+++ AI/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Entities> EntityList = SpawnEntities(20);
+            int counter = 0;
+
+
+            while (true)
+            {
+                List<Entities> born = new List<Entities>();
+                List<Entities> died = new List<Entities>();
+
+                foreach (Entities current in EntityList)
+                {
+                    current.doAge();
+                    if (current.isAlive())
+                    {
+                        foreach (Entities next in EntityList)
+                        {
+                            if (current != next)
+                            {
+                                born.Add(current.doBreed(next));
+                                current.doFight(next);
+                            }
+                        }
+                    }
+                    else
+                        died.Add(current);
+                }
+
+                born.RemoveAll(x => x == null);
+
+                foreach (Entities current in born)
+                    EntityList.Add(current);
+
+                foreach (Entities current in died)
+                    EntityList.Remove(current);
+
+                born.Clear();
+                died.Clear();
+
+                counter++;
+                Console.WriteLine("Round {0} has finished ! Total entities : {1} male, {2} female, ({3})!", counter, EntityList.Count(x => x.isMale()), EntityList.Count(x => !x.isMale()), EntityList.Count);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        static List<Entities> SpawnEntities(int Amount)
+        {
+            List<Entities> EntityList = new List<Entities>();
+            for(int i = 0; i<Amount; i++)
+            {
+                EntityList.Add(new Entities());
+            }
+
+            return EntityList;
+        }
+    }
+}
